95558d509f8e6159c3f52b04e16b9ee8317eb4ae
machadomariana513-wq <machadomariana513@gmail.com>
2026-03-03 09:07:32 -0300

    Update TV.java

--- TV.java
+++ TV.java
@@ -1,1 +1,42 @@
+class TV{
+  int tamanho de tela= 98;
+  int volume = 5;
+  int voltagem= 220;
+  int canal;
+  string marca;
 
+  int aumentarVolume(){
+     if (Volume < 10) {
+          Volume++;
+     }
+    return volume;
+  
+   }
+
+   void ligar (){
+    consumo= voltagem * tamanhodetela;
+    printf ("o consumo é igual a %d\n", consumo); 
+  
+  
+   }
+  
+  
+  
+  
+  }
+
+
+
+
+
+
+
+
+
+
+A televisão pode realizar as seguintes ações:
+
+ligar: ao ligar a televisão deve imprimir seu consumo. O consumo deve ser definido pela voltagem multiplicada pela quantidades de polegadas;
+desligar;
+aumentar e diminuir o volume;
+subir e descer canal.
